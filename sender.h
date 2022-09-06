40116b3b42fa01ba9e1358c803b27bc8b81aa34a
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-06 22:16:51 +0530

    Update sender.h

--- sender.h
+++ sender.h
@@ -1,1 +1,13 @@
 
+#pragma once
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+
+#define ROUND_OFF_TO_2_DECIMAL(Num)   (floor((Num)*100)/100)
+#define SIZE_OF_ARRAY(array)  (sizeof(array)/sizeof(array[0]))
+
+typedef struct{
+  float slope;
+  float xIntercept;
+}linerExpParameters;
