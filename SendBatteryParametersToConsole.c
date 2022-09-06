e92efd457a08ae03b02c03af434868fb3445f636
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-06 21:15:37 +0530

    Create SendBatteryParametersToConsole.c

---
+++ SendBatteryParametersToConsole.c
@@ -0,0 +1,11 @@
+#include "GenerateBatteryParameters.h"
+
+int main()
+{
+ /*char ToConsole[2000];
+ int NumOfValuesToBeGenerated = 50;
+ 
+ GenerateCurrentTemperaturePairToCsv(ToConsole, NumOfValuesToBeGenerated);
+ printf("%s",ToConsole);*/
+ return 0; 
+}
