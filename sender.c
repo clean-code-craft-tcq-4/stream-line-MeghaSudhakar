363dacfb4dc3f15143e0ee64cbf6516c7f154020
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-06 22:19:18 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -1,1 +1,21 @@
 
+void GenerateCurrentTemperaturePairToCsv(char* ToConsole, int NumOfValuesToBeGenerated)
+{
+  //char Tempchar[50] = "Current in A, Temperature in degC\n";
+  char Tempchar[50] = {0};
+  int Loop;
+  float CurrentArray[NumOfValuesToBeGenerated];
+  float TemperatureArray[NumOfValuesToBeGenerated];
+  
+  GenerateChargingCurrentValues(CurrentArray, NumOfValuesToBeGenerated);
+  GenerateBatteryTemperatureValues(TemperatureArray, NumOfValuesToBeGenerated);
+  
+  //strcat(ToConsole, Tempchar); // copy the header first
+  for(Loop = 0; Loop < NumOfValuesToBeGenerated;Loop++)
+  {
+    sprintf(Tempchar, "%0.2f, %0.2f\n",TemperatureArray[Loop],CurrentArray[Loop]);
+    //Data is sent to stdout buffer
+    //printf(ToConsole, " Temperature: %.2f, Charge Rate: %.2f\n", TemperatureArray[Loop], CurrentArray[Loop]);
+    strcat(ToConsole, Tempchar);
+  }
+}
