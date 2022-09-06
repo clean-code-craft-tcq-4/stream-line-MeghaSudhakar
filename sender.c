e87c23bedfcf03794279c9a130c2b3024b563edb
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-06 22:31:49 +0530

    Update sender.c

--- sender.c
+++ sender.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <stdio.h>
 
+void GenerateLinearExpValues(float* RandomData, linerExpParameters LinearExpressionParam, int NumOfValues)
+{
+  float y;
+  int LoopCount = 0;
+  int x = 0;
+  
+  for(LoopCount = 0; LoopCount < NumOfValues; LoopCount++)
+  {
+    x = LoopCount;
+    y = ((LinearExpressionParam.slope*x) + LinearExpressionParam.xIntercept); // y = mx+c
+    RandomData[LoopCount] = ROUND_OFF_TO_2_DECIMAL(y);
+  }
+  
+}
 
 void GenerateChargingCurrentValues(float* RandomData, int NumOfValuesToBeGenerated)
 {
