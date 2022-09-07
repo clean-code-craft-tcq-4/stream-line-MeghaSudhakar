2bd5ca24e37d23048aabec935002eb02a729107e
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-07 20:38:43 +0530

    Update test_GenerateChargingParameters.cpp

--- test_GenerateChargingParameters.cpp
+++ test_GenerateChargingParameters.cpp
@@ -39,3 +39,14 @@ TEST_CASE("To check if charging current is generated")
   
   REQUIRE(memcmp(RandomData, ExpectedData, sizeof(RandomData)) == 0);
 }
+
+TEST_CASE("To check if charging current is generated")
+{
+  float RandomData[10];
+  float ExpectedData[10] = {0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5}; // with slope = 0.5 and xIntercept = 0
+  int NumOfValuesToBeGenerated = SIZE_OF_ARRAY(RandomData);
+  
+  GenerateChargingCurrentValues(RandomData, NumOfValuesToBeGenerated);
+  
+  REQUIRE(memcmp(RandomData, ExpectedData, sizeof(RandomData)) == 0);
+}
