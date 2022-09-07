dadbab3d0e631a9b8fa0bb2b98448a8569163387
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-09-07 13:54:37 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -18,6 +18,19 @@ TEST_CASE("To test if 10 random values are generated from function GenerateLinea
   REQUIRE(memcmp(RandomData, ExpectedData, sizeof(RandomData)) == 0);
 }
 
+TEST_CASE("To test if 10 random values are generated from function GenerateLinearExpValues")
+{
+  float RandomData[10];
+  float ExpectedData[10] = {0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8}; // with slope = 0.2 and xIntercept = 2
+  int NumOfValuesToBeGenerated = SIZE_OF_ARRAY(RandomData);
+  linerExpParameters LinearExpressionParam = {0.2, 2.0}; // Slope, xIntercept
+
+  GenerateLinearExpValues(RandomData, LinearExpressionParam, NumOfValuesToBeGenerated);
+  
+  REQUIRE(memcmp(RandomData, ExpectedData, sizeof(RandomData)) == 0);
+}
+
+
 TEST_CASE("To check if charging current is generated")
 {
   float RandomData[10];
